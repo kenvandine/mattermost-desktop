c14f90de2aafa22b2f81e2d576f7af3b0449db37
Ken VanDine <ken.vandine@canonical.com>
2025-09-19 04:16:53 -0400

    feat: Enable wayland (#135)
    * feat: Enable wayland

--- snap/local/launcher.sh
+++ snap/local/launcher.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-exec "$SNAP/opt/Mattermost/mattermost-desktop" --no-sandbox --disable-seccomp-filter-sandbox --disable-features=GlobalShortcutsPortal "$@"
+
+if [ -n "$WAYLAND_DISPLAY" ] && [ -z "$DISABLE_WAYLAND" ]; then
+  WAYLAND_OPTS="--enable-features=UseOzonePlatform --ozone-platform-hint=auto"
+fi
+
+exec "$SNAP/opt/Mattermost/mattermost-desktop" --no-sandbox --disable-seccomp-filter-sandbox --disable-features=GlobalShortcutsPortal "${WAYLAND_OPTS:-}" "$@"
